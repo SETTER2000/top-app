cea9f10a644018dfe46c46c368346f5ae8f0acdf
SETTER <lphp@mail.ru>
2025-07-26 22:41:02 +0300

    Init App

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import {JSX} from "react";
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
+
