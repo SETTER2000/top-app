5adc7df418039445fae336f5f9776913870ac2ea
SETTER <lphp@mail.ru>
2026-01-15 21:55:01 +0300

    feat: изменить метаданные author

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import {JSX} from "react";
+import Head from "next/head";
+import { Noto_Sans_Display } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+const notoSans = Noto_Sans_Display({
+ subsets: ["cyrillic", "latin"], // Выберите нужные подмножества
+ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+ display: "swap",
+});
+
+export default function App({ Component, pageProps }: AppProps){
+	return <>
+  <Head>
+   <title>MyTop - наш лучший топ </title>
+   <link rel="icon" href="/favicon.ico" />
+
+  </Head>
+  <div className={notoSans.className}>
+   <Component {...pageProps} />
+  </div>
+ </>;
 }
 
